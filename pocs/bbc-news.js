50e773709242491de76f4b3c527a770ddc462dc1
Dinis Cruz <dinis.cruz@owasp.org>
2016-10-07 13:19:38 +0100

    Update bbc-news.js

--- pocs/bbc-news.js
+++ pocs/bbc-news.js
@@ -5,4 +5,4 @@ setTimeout( function() {
   doc.getElementsByClassName('title-link__title-text')[0].innerHTML='Secure BBC ID Launched';
   doc.getElementsByClassName('buzzard__summary')[0].innerHTML = "Today BBC is launching its new BBC ID which is super secure, and is able to protect the user's data using state of the art security code."
   doc.getElementsByClassName('buzzard__image')[0].getElementsByTagName('img')[0].src='http://www.bbc.co.uk/staticarchive/bd750f494890387769963209883191f0e0ec9ed3.jpg'
- }, 400)
+ }, 600)
