e8eef2c8efd355bd982c3eb32e293b51bcf02ed7
Dinis Cruz <dinis.cruz@owasp.org>
2017-02-16 13:48:03 +0000

    Update bbc-news.js

--- pocs/bbc-news.js
+++ pocs/bbc-news.js
@@ -3,6 +3,6 @@ document.body.innerHTML='<iframe id="exploit" src="' + url + '" width="100%" hei
 setTimeout( function() { 
   var doc = frames['exploit'].contentWindow.document;
   doc.getElementsByClassName('gs-c-promo-heading__title')[0].innerHTML='Secure BBC ID Launched';
-  doc.getElementsByClassName('buzzard__summary')[0].innerHTML = "Today BBC is launching its new BBC ID which is super secure, and is able to protect the user's data using state of the art security code."
-  doc.getElementsByClassName('buzzard__image')[0].getElementsByTagName('img')[0].src='http://www.bbc.co.uk/staticarchive/bd750f494890387769963209883191f0e0ec9ed3.jpg'
+  doc.getElementsByClassName('gs-c-promo-summary')[0].innerHTML = "Today BBC is launching its new BBC ID which is super secure, and is able to protect the user's data using state of the art security code."
+  doc.getElementsByClassName('gs-o-responsive-image')[0].getElementsByTagName('img')[0].src='http://www.bbc.co.uk/staticarchive/bd750f494890387769963209883191f0e0ec9ed3.jpg'
  }, 600)
